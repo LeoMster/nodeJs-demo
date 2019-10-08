0c65aed43f1f753a39022b9b461d65098ee31ee5
lihang <37512653@qq.com>
2019-10-08 17:16:49 +0800

    node-code

--- 16-http-server/04-response.js
+++ 16-http-server/04-response.js
@@ -8,6 +8,20 @@ http.createServer((req,res)=>{
   console.log(req.headers);
 
   cosole.log();
+  
+  req.pipe(process,stdout);
+  
+  var html = ;
 
+  if(req.url === '/'){
+    //200
+    res.writeHead(200,{'Content-Type':'text/html','Content-length:',Buffer.byteLength(html,'utf8')})
+    res.end(html);
+  }else{
+    // 404
+    res.statusCode = 404;
+    res.setHeader('Content-Type','text/plain');
+    res.end('error');
+  }
   res.end('hello world!');
 }).listen(8080);
---
+++ 16-http-server/05-form-get.js
@@ -0,0 +1,59 @@
+#!/usr/bin/node
+
+const http = require('http'),
+      qs = require('querystring'),
+      url = require('url');
+
+var items = [];
+
+http.createServer((req,res)=>{
+  console.log(`${req.method} ${req.url}`);
+
+  console.log(req.headers);
+
+  cosole.log();
+  
+  req.pipe(process,stdout);
+  
+  function getHtml(){
+  return ''+
+    +'<!DOCTYPE html>'
+    + '<html lang="en">'
+    +'<head>'
+    +'<meta charset="UTF-8">'
+    +'<title>Document</title>'
+    +'</head>'
+    +'<body>'
+    +'<h1>TODO list</h1>'
+  +'<ul>'
+  +items.map(function(it){
+    return '<li>'+it+'</li>';
+  }).join('\n');
+  +'</ul>'
+    +'<form method="GET" action="/">'
+    +'<input type="text" name="items">'
+    +'<input type="submit" value="提交">'
+    +'</form>'
+    +'</body>'
+    +'</html>'
+}
+  if(req.url === '/'){
+    //200
+    res.writeHead(200,{'Content-Type':'text/html','Content-length:',Buffer.byteLength(html,'utf8')})
+    res.end(getHtml());
+  }else{
+    // 404
+    var it = qs.parse(url.parse(eq.url).query));
+
+    if(typeof it === undefined){
+      items.push(it);
+    }
+
+
+
+   // res.statusCode = 404;
+    //es.setHeader('Content-Type','text/plain');
+    //res.end('error');
+  }
+  res.end('hello world!');
+}).listen(8080);
